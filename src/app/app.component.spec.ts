c399da2c43b9ddfc4eb08b767751e53371b0069f
Yunus ÖLMEZ <yns4832@hotmail.com>
2021-05-23 09:00:18 +0300

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'backbase-assignment'`, () => {
+  it(`should have as title 'Backbase Assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('backbase-assignment');
+    expect(app.title).toEqual('Backbase Assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('backbase-assignment app is running!');
+    expect(compiled.querySelector('header span.fs-3').textContent).toContain('Backbase Weather Forecast');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'backbase-assignment';
+  title = 'Backbase Assignment';
 }
