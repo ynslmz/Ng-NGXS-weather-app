f82248c9490c9ada326ac56a918afb2a985ed27e
Yunus ÖLMEZ <yns4832@hotmail.com>
2021-05-30 18:16:36 +0300

    remove commented specs Add take pipes test

--- src/app/shared/pipes/take/take.pipe.spec.ts
+++ src/app/shared/pipes/take/take.pipe.spec.ts
@@ -1,8 +1,22 @@
-// import { TakePipe } from './take.pipe';
+import { TakePipe } from './take.pipe';
 
-// describe('TakePipe', () => {
-//   it('create an instance', () => {
-//     const pipe = new TakePipe();
-//     expect(pipe).toBeTruthy();
-//   });
-// });
+describe('TakePipe', () => {
+  const array = [1, 2, 3, 4, 5, 6]
+  it('create an instance', () => {
+    const pipe = new TakePipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should transform length to 1', () => {
+    const pipe = new TakePipe();
+    let newArray = pipe.transform(array, [1]);
+    expect(newArray.length).toBeTruthy(1);
+  });
+
+  it('should return all items when take comes bigger (9 -> 6)', () => {
+    const pipe = new TakePipe();
+    let newArray = pipe.transform(array, [9]);
+    expect(newArray.length).toBeTruthy(6);
+  });
+
+});
